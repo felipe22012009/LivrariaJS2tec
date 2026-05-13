30892a79528b8945dce9d695927c6a988e89ac1f
Fernando Felipe <fernando.silva.ribeiro@escola.pr.gov.br>
2026-05-13 13:01:08 +0000

    ok

---
+++ listaLivros.js
@@ -0,0 +1,45 @@
+let precoLivros = [
+    {
+        titulo: "Do Dia para a Noite",
+        preco: 45
+    },
+     {
+        titulo: "Dias Quentes",
+        preco: 39
+    },
+     {
+        titulo: "Café com Deus Pai 2026",
+        preco: 55
+    },
+     {
+        titulo: "Verity ",
+        preco: 35
+    },
+     {
+        titulo: "A Psicologia Financeira",
+        preco: 40
+    },
+     {
+        titulo: "A Metamorfose ",
+        preco: 20
+    },
+     {
+        titulo: "Hábitos Atômicos",
+        preco: 42
+    },
+     {
+        titulo: "A Hora da Estrela ",
+        preco: 25
+    },
+     {
+        titulo: "Isso e Aquilo",
+        preco: 30
+    },
+     {
+        titulo: "O Homem Mais Rico da Babilônia ",
+        preco: 18
+    },
+
+    
+]
+module.exports = precoLivros;
